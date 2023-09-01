fbbabb025799500652d5a3935f2a419df52afc73
Davyd <davidkrivko@gmail.com>
2023-09-02 01:40:06 +0200

    Add admin models

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import CheckModel, PrinterModel
 
-# Register your models here.
+
+class CheckAdmin(admin.ModelAdmin):
+    list_filter = ("printer", "status", "type")
+
+
+class PrinterAdmin(admin.ModelAdmin):
+    list_filter = ("check_type", "point_id")
+
+
+admin.site.register(CheckModel, CheckAdmin)
+admin.site.register(PrinterModel, PrinterAdmin)
