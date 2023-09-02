eabd69bdd72ae616525c35f973106d324465ab1a
Davyd <davidkrivko@gmail.com>
2023-09-02 15:48:58 +0200

    Add media file url

--- food_service/urls.py
+++ food_service/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
@@ -38,4 +39,5 @@ if settings.DEBUG:
     urlpatterns += [
         path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
         path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     ]
